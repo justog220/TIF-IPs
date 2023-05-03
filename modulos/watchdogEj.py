559eb7f578318dac5f50b82ca9479ccb418b30f6
Justo Garcia <justogarciapc@gmail.com>
2023-05-03 19:13:53 -0300

    1er commit

---
+++ modulos/watchdogEj.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  3 17:42:15 2023
+
+@author: justo
+"""
+
+import time 
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class WatchdogSSH(FileSystemEventHandler):
+    def on_created(self, event):
+        print(f"Evento {event.event_type} detectado en {event.src_path}")
+        if '../pruebaTxt/datos.txt' == event.src_path:
+            print(f"Archivo {event.src_path} modificado")
+            
+    def on_modified(self, event):
+        print(f"Evento {event.event_type} detectado en {event.src_path}")
+        if '../pruebaTxt/datos.txt' == event.src_path:
+            print(f"Archivo {event.src_path} modificado")
+            
+if __name__ == "__main__":
+    event_handler = WatchdogSSH()
+    
+    observer = Observer()
+    
+    observer.schedule(event_handler, path='../pruebaTxt/datos.txt', recursive=False)
+    
+    observer.start()
+    
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+        
+        
+    observer.join()
+                
