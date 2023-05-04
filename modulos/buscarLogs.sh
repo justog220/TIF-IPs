c83244d629cdab5a84dffe4f70a74fafc9dc19c2
justog220 <justogarciapc@gmail.com>
2023-05-04 17:45:07 -0300

    Parser

--- modulos/buscarLogs.sh
+++ modulos/buscarLogs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ruta_actual=$(pwd)
-cat /var/log/auth.log | grep sshd | grep -Eo ' [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} ' | grep -v '0.0.0.0' | grep -v '127.0.0.1'  > $ruta_actual/data/ips.txt
-cat /var/log/auth.log | grep sshd | grep -E ' [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} ' | grep -v '0.0.0.0' | grep -v '127.0.0.1' | awk '{print $1" "$2" "$3" "}' > $ruta_actual/data/hora.txt
-paste $ruta_actual/data/ips.txt $ruta_actual/data/hora.txt > $ruta_actual/data/hora_ip.txt
+cat /var/log/auth.log | grep sshd | grep -Eo ' [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -v '0.0.0.0' | grep -v '127.0.0.1'  > $ruta_actual/data/ips.txt
+cat /var/log/auth.log | grep sshd | grep -E ' [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -v '0.0.0.0' | grep -v '127.0.0.1' | awk '{print $1" "$2" "$3}' > $ruta_actual/data/hora.txt
+paste $ruta_actual/data/hora.txt $ruta_actual/data/ips.txt > $ruta_actual/data/hora_ip.txt
 rm $ruta_actual/data/ips.txt $ruta_actual/data/hora.txt
