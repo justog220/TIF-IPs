56c306a2ed5f961ae478371fb1faabd5e8b33bc0
justog220 <justogarciapc@gmail.com>
2023-05-15 11:54:16 -0300

    Soluciono sudo

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
+#!/bin/bash
+
 carpeta=$(basename $(pwd))
 if [[ $carpeta == "TIF-IPs" ]]
 then
     if [ "$UID" -eq 0 ]; then
-        sudo sh -c 'cd app/modulos'
+        sudo sh -c 'cd app/modulos; python3 watchdogEj.py'
     else
         cd app/modulos
+        python3 watchdogEj.py
     fi
 fi
 
-python3 watchdogEj.py
+
